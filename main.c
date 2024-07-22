3fabd6ab5994cbffb2026fd40918515c37174ccc
JordanHolberton <jordan.chatillon76@laposte.net>
2024-07-22 13:49:11 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * 
+ * Return: Always 0
+ */
+
+int main(void)
+{
+    char chiffre = "50";
+    char ligne = "Quel âge avez-vous?";
+    char lettre = "A"
+    _printf("%d, %s, %c", chiffre, ligne, lettre);
+    return(0);
+}
