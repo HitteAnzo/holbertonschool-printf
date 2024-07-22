4df8779752238117d2252efca0957f2ff8efb3b4
HitteAnzo <hitteanzo@gmail.com>
2024-07-22 20:42:58 +0200

    Update _str.c

---
+++ _str.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * 
+ */
+
+int _str(va_list args)
+{
+    int i = 0;
+    char *str = va_args(args, char*);
+
+    if (str == NULL)
+    {
+        _putchar('N');
+        _putchar('U');
+        _putchar('L');
+        _putchar('L');
+        return (4);
+    }
+
+    while (str[i] != '\0')
+    {
+        _putchar(str[i]);
+        i++;
+    }
+    return (i);
+}
