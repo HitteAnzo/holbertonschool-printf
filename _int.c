66388819802476d50d39b78c9ba90a3103d76601
JordanHolberton <jordan.chatillon76@laposte.net>
2024-07-23 11:29:00 +0200

    Add function for print integers

---
+++ _int.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _int - print integer
+ * @args: arguments
+ * 
+ * Return: count chars
+ */
+
+int _int(va_list args)
+{
+    long int num = va_args(args,int);
+    int numcount = 0;
+    int mul = 1;
+    int div;
+
+    if (num < 0)
+    {
+        numcount += _putchar('-');
+        num *= -1;
+    }
+
+    if (num < 10)
+        return (numcount += _putchar(digit + '0'));
+
+    div = num
+
+    while (div > 9)
+    {
+        mul *= 10;
+        div /= 10;
+    }
+    while (mul >= 1)
+    {
+        numcount += _putchar(((num / div) % 10) + 0);
+        mul /= 10;
+    }
+
+    return (numcount);
+}
