22c4b3a81bc09bba527d8e719b1bd020a46fc955
JordanHolberton <jordan.chatillon76@laposte.net>
2024-07-23 15:00:26 +0200

    first try for printf file

---
+++ _printf.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _printf - printf command
+ * 
+ * @format: specifier
+ * 
+ * Return: numcount
+ */
+
+int _printf(const char *format, ...)
+{
+    int i = 0, count = 0, numcount;
+    va_list args;
+
+    va_start(args, format);
+    if (!format || (format[0] == '%' && !format[1])) /*check if format is '%' followed by null */
+        return (-1);
+    if (format[0] = '%' && format[1] = ' ' && !format[2]) /*check if format is '%' followed by space and null*/
+        return (-1);
+
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            numcount += _format(format[i + 1], args);/*take the specifier*/
+            i++;
+        }
+        else
+        {
+            numcount++;
+            _putchar(format[i]);
+        }
+    }
+    va_end(args);
+    return(numcount);
+}
