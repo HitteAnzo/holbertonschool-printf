4fbc7a54e0fb7942004b7665e3c3ce93b2d66056
HitteAnzo <hitteanzo@gmail.com>
2024-07-24 11:33:49 +0200

    Fix betty _format

--- _format.c
+++ _format.c
@@ -1,34 +1,33 @@
 #include "main.h"
 
 /**
- * _format - function specifiers
- * @type: conversion specifiers
- * @args: arguments
- * 
- * Return: numcount
+ * _format - function specifiers.
+ * @type: conversion specifiers.
+ * @args: arguments.
+ * Return: numcount.
  */
 int _format(const char type, va_list args)
 {
-    int i = 0;
+	int i = 0;
 
-    specifiers_t spec[] = {
-        {'c', _char},
-        {'s', _str},
-        {'%', _percent},
-        {'d', _int},
-        {'i', _int},
-        {0, NULL}
-    };
+	specifiers_t spec[] = {
+		{'c', _char},
+		{'s', _str},
+		{'%', _percent},
+		{'d', _int},
+		{'i', _int},
+		{0, NULL}
+	};
 
-    while(spec[i].specifiers)
-    {
-        if(type == spec[i].specifiers)
-            return(spec[i].f(args));
-        i++;
-    }
+	while(spec[i].specifiers)
+	{
+		if(type == spec[i].specifiers)
+			return(spec[i].f(args));
+		i++;
+	}
 
-    _putchar('%');
-    _putchar(type);
+	_putchar('%');
+	_putchar(type);
 
-    return(2);
+	return(2);
 }
