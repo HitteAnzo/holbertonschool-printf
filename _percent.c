197cdab3c945d728e032a84cfb335b3574e2c5e5
Hitte Anzo <166728678+HitteAnzo@users.noreply.github.com>
2024-07-24 16:49:30 +0200

    Update _percent.c

--- _percent.c
+++ _percent.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * _percent - print %
- * @args: list of arguments
- * Return: number of chars
+ * _percent - print %.
+ * @args: list of arguments.
+ * Return: number of chars.
  */
-
 int _percent(va_list args)
 {
 	(void)args;
